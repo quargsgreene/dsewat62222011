c15abe722195feacd2cf29bf2c2884b12bd7dab1
Quargs Greene <quargsgreene@gmail.com>
2022-12-01 22:52:59 -0800

    added message and changed up background aesthetics

--- sketch.js
+++ sketch.js
@@ -1,45 +1,42 @@
 let img;
-let b=20;
-let fr=1;
+const backgroundColor = 0;
 
-function preload(){
-  img=loadImage("ss.png");
-  
+function preload() {
+  img = loadImage('backgroundImage.png');
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  smallBlob=2;
-  largeBlob=20;
+  createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
   strokeWeight(0.2);
-  stroke(0,50);
-  background(b);
+  stroke(0, 50);
+  background(backgroundColor);
   img.loadPixels();
-  frameRate(fr);
-  
+
+  const message = createP('Congratulations!\n You have either made it to the end or decided to click here for some reason.\n You do not win a puppy.');
+  message.position(0, height / 3);
+  message.class('invisible');
 }
 
-
 function draw() {
-  
-  let x = floor(random(img.width));
-  let y = floor(random(img.height));
-  let pix = img.get(x,y);
-
-  for(let i=0;i<width;i+=10){
-    for(let j=0;j<height;j+=10){
+  for (let i = 0; i < width; i += 50) {
+    for (let j = 0; j < height; j += 50) {
+      const pix = img.get(i, random(j));
       push();
-      fill(pix,100);
-      rect(i,j,2,4);
-      ellipse(i,j,4,4);
+      fill(pix, 100);
+      rect(i, j, 20, 200);
+      noStroke();
+      ellipse(i, j, 40, 200);
       pop();
-      fill(b,200);
-      rect(i,j,2,4);
-      rect(i,j,2,2);
-      ellipse(i,j,4,4);
-      ellipse(i,j,4,2);
-      
+      fill(backgroundColor);
+      rect(i, j, 20, 40);
+      rect(i, j, 20, 20);
+      ellipse(i, j, 40, 40);
+      ellipse(i, j, 40, 20);
     }
   }
+
+  fill(255);
+  textSize(36);
+  text(' Congratulations!\n You have either made it to the end ordecided to click here for some reason.\n You do not win a puppy.', 0, height / 2);
 }
